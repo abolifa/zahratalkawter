cff7b3ccbea1a0b9f3d8883eb0be12528abc4bfb
Abulifa <abulifa@outlook.com>
2025-11-15 19:40:30 +0200

    chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+// vite.config.ts
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -17,11 +17,10 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
             if (id.includes("framer-motion")) return "motion";
             if (id.includes("lucide-react")) return "icons";
             if (id.includes("i18next")) return "i18n";
-            return "vendor";
+            return "vendor"; // contains ALL react packages safely
           }
         },
       },
